86938fa73fbfc63170aa0cf1a7053aac77db1e9b
bala-amypo <bala@amypo.in>
2025-12-17 09:55:56 +0000

    User commit

--- src/main/java/com/example/demo/entity/student.java
+++ src/main/java/com/example/demo/entity/student.java
@@ -1,39 +1,40 @@
-package com.example.project.entity;
+package com.example.sql.Entitydata;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@entity
-public class student{
-    @id
-    @GeneratedValue(strategy = GenerationType.INDENTITY)
+@Entity
+public class student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
-
-public Integer getid(){
-    return id;
-}
-public void setId(Integer id){
-    this.id=id;
-}
-public String getName(){
-    return name;
-}
-public void setName(String name){
-    this.name=name;
-}
-public String getEmail(){
-    return email;
-}
-public void setEmail(String email){
-    this.email=email;
-}
-public student(Integer id,String name,String email){
-    this.id=id;
-    this.name=name;
-    this.email=email;
-}
-public student(){
-
-}
+    public int getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Student(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    public Student() {
+    }
 }
